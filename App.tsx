483e28cf07846ac5e0c4c93a861c7d0d734b5790
cotete <feliperibeirots@gmail.com>
2025-05-05 01:53:20 -0300

    Adicionado as paginas e navegações para elas

--- App.tsx
+++ App.tsx
@@ -2,8 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles/styles';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, ParamListBase } from '@react-navigation/native';
 import Inicio from './pages/Inicio/Inicio';
+import Mapa from './pages/Mapa/Mapa';
+import ProcurarMoto from './pages/ProcurarMoto/ProcurarMoto';
+import AdicionarRastreador from './pages/AdicionarRastreador/AdicionarRastreador';
 
 export default function App() {
 
@@ -13,17 +16,33 @@ export default function App() {
     <NavigationContainer>
       <Navigator initialRouteName='Inicio' screenOptions={{
         headerStyle:{backgroundColor:"#2C2C2C"},
+        headerTitleStyle:{
+          fontSize:24
+        },
         drawerActiveBackgroundColor: '#41C52620',
         drawerLabelStyle:{
-          color:"#41C526"
+          color:"#41C526",
+          fontSize:18
         },
         headerTintColor:"#41C526",
         drawerStyle:{
-          backgroundColor:"#2C2C2C"
+          backgroundColor:"#2C2C2C",
         }
       }}>
-      <Screen name='Inicio' component={Inicio}>
+      <Screen name='Inicio'>
+        {(props:ParamListBase)=><Inicio {...props}></Inicio>}
       </Screen>
+      <Screen name='Mapa'>
+        {(props:ParamListBase)=><Mapa {...props}></Mapa>}
+      </Screen>
+      <Screen name='Procurar Moto'>
+        {(props:ParamListBase)=><ProcurarMoto {...props}></ProcurarMoto>}
+      </Screen>
+      <Screen name='Adicionar Rastreador'>
+        {(props:ParamListBase)=><AdicionarRastreador {...props}></AdicionarRastreador>}
+      </Screen>
+
+      
 
       </Navigator>
     </NavigationContainer>
---
+++ pages/Filial/Filial.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export default function Filial() {
+  return (
+    <View>
+        <Text>Filial</Text>
+    </View>
+  )
+}
