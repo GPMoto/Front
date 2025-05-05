430be1bde30fe304fa738a6447a4c4fce3e62909
cotete <feliperibeirots@gmail.com>
2025-05-05 01:42:11 -0300

    Instalado o drawer e iniciada a pagina inicio

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { styles } from './styles/styles';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Inicio from './pages/Inicio/Inicio';
 
 export default function App() {
+
+  const {Screen, Navigator} = createDrawerNavigator()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator initialRouteName='Inicio' screenOptions={{
+        headerStyle:{backgroundColor:"#2C2C2C"},
+        drawerActiveBackgroundColor: '#41C52620',
+        drawerLabelStyle:{
+          color:"#41C526"
+        },
+        headerTintColor:"#41C526",
+        drawerStyle:{
+          backgroundColor:"#2C2C2C"
+        }
+      }}>
+      <Screen name='Inicio' component={Inicio}>
+      </Screen>
+
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
