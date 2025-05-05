78b78cc5ebe86f0edfc389f5c500ac193c233a24
cotete <feliperibeirots@gmail.com>
2025-05-05 16:57:44 -0300

    feito o options input

--- App.tsx
+++ App.tsx
@@ -41,7 +41,6 @@ export default function App() {
       <Screen name='Adicionar Rastreador'>
         {(props:ParamListBase)=><AdicionarRastreador {...props}></AdicionarRastreador>}
       </Screen>
-
       
 
       </Navigator>
---
+++ Util/Interfaces.ts
@@ -0,0 +1,3 @@
+export interface optionsInterface{
+    value:string
+}
