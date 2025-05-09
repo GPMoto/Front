b049b44d7246310879c9098d851c04395fbca0fa
gustavodsdcruz <gustavodiasdsc@gmail.com>
2025-05-09 10:40:24 -0300

    Update .gitignore and Moto.ts types

--- utils/types/Moto.ts
+++ utils/types/Moto.ts
@@ -1,14 +1,15 @@
 export interface MotoView {
-    id? : number;
-    uwb : number;
-    color : string;
-    motoData : MotoData;
+    id?: number;
+    uwb: number;
+    color: string;
+    motoData: MotoData;
+    clicked: boolean;
 }
 
 export interface MotoData {
     id: number;
     uwb: number;
-    nome : string;
-    status : string;
-    identificador : string;
+    nome: string;
+    status: string;
+    identificador: string;
 }
