748c869661fc145c8eb0fb86bd07b63f9df491ef
gustavodscruz <gustavodiasdsc@gmail.com>
2025-05-08 22:57:55 -0300

    MapaComponent na página de mapa

--- pages/Inicio/Inicio.tsx
+++ pages/Inicio/Inicio.tsx
@@ -1,14 +1,14 @@
-import { Text, View } from 'react-native'
-import { styles } from '../../styles/styles'
-import Mapa from '../../components/MapaComponent/MapaComponent'
+import { Text, View } from "react-native";
+import { styles } from "../../styles/styles";
+import MapaComponent from "../../components/MapaComponent/MapaComponent";
+import { ScrollView } from "react-native";
 
 export default function Inicio() {
   return (
     <View style={styles.container_center}>
-        <Text style={styles.paragraph}>Olá, seja bem vindo de volta</Text>
-        <Text style={styles.highlight}>Gustavo</Text>
-        <View style={{height: 20}}></View>
-        <Mapa /> 
+       
+        <View style={{ height: 20 }}></View>
+        <MapaComponent />
     </View>
-  )
+  );
 }
