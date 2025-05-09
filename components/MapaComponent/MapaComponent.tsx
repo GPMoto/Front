38efe403ef1fc775ae5c8ccbc2ff14c152030d37
gustavodscruz <gustavodiasdsc@gmail.com>
2025-05-08 22:57:55 -0300

    MapaComponent na página de mapa

--- components/MapaComponent/MapaComponent.tsx
+++ components/MapaComponent/MapaComponent.tsx
@@ -1,7 +1,14 @@
 import Fontisto from "@expo/vector-icons/Fontisto";
 import React, { useState } from "react";
-import { FlatList, ListRenderItemInfo, Text, ToastAndroid, View } from "react-native";
+import {
+  FlatList,
+  ListRenderItemInfo,
+  Text,
+  ToastAndroid,
+  View,
+} from "react-native";
 import { styles } from "../../styles/styles";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 type Moto = {
   id: number;
@@ -9,15 +16,16 @@ type Moto = {
   color: string;
 };
 
-export default function Mapa() {
-  const obj: Moto = {
-    id: Math.floor(Math.random() * 10000),
-    uwb: Math.floor(Math.random() * 10000),
-    color: "black",
-  };
+export default function MapaComponent() {
+  // const obj: Moto = {
+  //   id: Math.floor(Math.random() * 10000),
+  //   uwb: Math.floor(Math.random() * 10000),
+  //   color: "black",
+  //   clicked: false
+  // };
 
   const inicializarMotos = (): Moto[] => {
-    return Array.from({ length: 60 }, () => ({
+    return Array.from({ length: 100 }, () => ({
       id: Math.floor(Math.random() * 10000),
       uwb: Math.floor(Math.random() * 10000),
       color: "black",
@@ -41,16 +49,14 @@ export default function Mapa() {
   return (
     <View
       style={{
-        borderWidth: 3,
-        borderColor: "#41C526",
+        // borderWidth: 1,
+        // borderColor: "#41C526",
         backgroundColor: "#D9D9D9",
-        borderRadius: 16,
-        padding: 20,
-        gap: 15,
-        maxWidth: 550,
-        width: "80%",
-        alignItems: 'center',
-        alignSelf: 'center'
+        borderRadius: 4,
+        padding: 10,
+        gap: 2,
+        maxWidth: "90%",
+        alignSelf: "center",
       }}
     >
       <Text style={styles.paragraph_black}>Pátio</Text>
@@ -61,27 +67,177 @@ export default function Mapa() {
           borderColor: "black",
           borderWidth: 1,
           flexWrap: "wrap",
-          width: "80%",
-          maxWidth: 500,
-          alignSelf: "center",
           flexDirection: "row",
           gap: 20,
           justifyContent: "center",
-          height: 200
+          height: 200,
+          alignSelf: "center",
         }}
       >
-        <FlatList style={{flexDirection: 'row'}} data={listaMotos} renderItem={({item, index} : ListRenderItemInfo<Moto>) => <Fontisto
-            name="motorcycle"
-            size={24}
-            color={item.color}
-            key={index}
-            horizontal
-            onPress={() => {
-              atualizarCorMoto(item.id)
-              ToastAndroid.show(`Moto selecionada ${item.id}: uwb ${item.uwb}!`, ToastAndroid.SHORT);
-
+        <FlatList
+          numColumns={8} // Define o número de colunas
+          data={listaMotos}
+          renderItem={({ item }: ListRenderItemInfo<Moto>) => (
+            <Fontisto
+              name="motorcycle"
+              size={20}
+              color={item.color}
+              onPress={() => {
+                atualizarCorMoto(item.id);
+                ToastAndroid.show(
+                  `Moto selecionada ${item.id}: uwb ${item.uwb}!`,
+                  ToastAndroid.SHORT
+                );
+              }}
+            />
+          )}
+          keyExtractor={(item: Moto) => "moto_key_" + item.id}
+          contentContainerStyle={{
+            gap: 10, // Espaçamento entre os itens
+            justifyContent: "center",
+          }}
+          columnWrapperStyle={{
+            justifyContent: "center", // Centraliza as colunas
+            gap: 20, // Espaçamento entre as colunas
+          }}
+          style={{
+            maxHeight: 180, // Limita a altura visível
+          }}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "stretch",
+          width: "100%",
+          gap: 10,
+        }}
+      >
+        <View style={{ paddingVertical: 4, gap: 2 }}>
+          <Text style={{ fontSize: 14, color: "black" }}>Conserto</Text>
+          <View
+            style={{
+              borderRadius: 8,
+              padding: 5,
+              borderColor: "black",
+              borderWidth: 1,
+              flexWrap: "wrap",
+              flexDirection: "row",
+              gap: 20,
+              justifyContent: "flex-start",
+              height: 100,
+              width: 100,
+              backgroundColor: "#A44949",
+              alignSelf: "center",
+              alignItems: "flex-start",
             }}
-          />} keyExtractor={(item : Moto) => "moto_key_" + item.id} /> 
+          >
+            <MaterialIcons name="report-problem" size={16} color="black" />
+          </View>
+        </View>
+        <View style={{ paddingVertical: 4, gap: 2 }}>
+          <Text style={{ fontSize: 14, color: "black" }}>Conserto</Text>
+          <View
+            style={{
+              borderRadius: 8,
+              padding: 5,
+              borderColor: "black",
+              borderWidth: 1,
+              flexWrap: "wrap",
+              flexDirection: "row",
+              gap: 20,
+              justifyContent: "flex-start",
+              flex: 1,
+              backgroundColor: "#49A44C",
+              alignSelf: "center",
+              alignItems: "flex-start",
+              height: 100,
+              width: 170
+            }}
+          >
+            <MaterialIcons name="report-problem" size={16} color="black" />
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: 10,
+        }}
+      >
+        <View style={{ height: "100%", alignItems: "stretch" }}>
+          <View style={{ paddingVertical: 4, gap: 2 }}>
+            <Text style={{ fontSize: 14, color: "black" }}>Administrativo</Text>
+            <View
+              style={{
+                borderRadius: 8,
+                padding: 5,
+                borderColor: "black",
+                borderWidth: 1,
+                flexWrap: "wrap",
+                flexDirection: "row",
+                gap: 20,
+                justifyContent: "flex-start",
+                width: 165,
+                backgroundColor: "#A4A449",
+                alignItems: "flex-start",
+                height: 100,
+              }}
+            >
+              <MaterialIcons
+                name="admin-panel-settings"
+                size={24}
+                color="black"
+              />
+            </View>
+          </View>
+          <View style={{ paddingVertical: 4, gap: 2 }}>
+            <Text style={{ fontSize: 14, color: "black" }}>Estoque</Text>
+            <View
+              style={{
+                borderRadius: 8,
+                padding: 5,
+                borderColor: "black",
+                borderWidth: 1,
+                flexWrap: "wrap",
+                flexDirection: "row",
+                gap: 20,
+                justifyContent: "flex-start",
+                width: 165,
+                backgroundColor: "#5049A4",
+                alignItems: "flex-start",
+                height: 100,
+              }}
+            >
+              <MaterialIcons name="content-paste" size={24} color="black" />
+            </View>
+          </View>
+        </View>
+        <View style={{alignItems: 'flex-start', height: '100%'}}>
+          <View style={{ paddingVertical: 4, gap: 2}}>
+            <Text style={{ fontSize: 14, color: "black" }}>Recepção</Text>
+            <View
+              style={{
+                borderRadius: 8,
+                padding: 5,
+                borderColor: "black",
+                borderWidth: 1,
+                flexWrap: "wrap",
+                flexDirection: "row",
+                gap: 20,
+                justifyContent: "flex-start",
+                width: 100,
+                height: 225,
+                backgroundColor: "#A4499E",
+                alignItems: "flex-start",
+              }}
+            >
+              <MaterialIcons name="person" size={24} color="black" />
+            </View>
+          </View>
+        </View>
       </View>
     </View>
   );
