3e59ffe3b76aff0f545a8d19b72517a17ed5752c
gustavodscruz <gustavodiasdsc@gmail.com>
2025-05-09 01:36:02 -0300

    relatorios para motos, componente dinamico de botao, modal para mapa/moto e mocks

---
+++ components/MapaComponent/ModalMapaComponent.tsx
@@ -0,0 +1,141 @@
+import { FontAwesome } from "@expo/vector-icons";
+import React from "react";
+import {
+  Button,
+  Image,
+  ImageSourcePropType,
+  Modal,
+  Text,
+  ToastAndroid,
+  View,
+} from "react-native";
+import { MotoData } from "../../utils/types/Moto";
+import ButtonArea from "../Button/ButtonArea";
+
+interface ModalMapaProps {
+  modalVisible: boolean;
+  setModalVisible: (value: boolean) => void;
+  motoData: MotoData;
+}
+
+export default function ModalMapaComponent({
+  modalVisible,
+  setModalVisible,
+  motoData,
+}: ModalMapaProps) {
+  const catchRightImage = {
+    "Mottu Sport": require(`../../assets/sport.png`),
+    "Mottu E": require(`../../assets/e.png`),
+    "Mottu Pop": require(`../../assets/pop.png`),
+  };
+
+  const rightName = (motoName: string) => {
+    switch (motoName) {
+      case "Mottu Sport":
+        return require(`../../assets/sport.png`);
+      case "Mottu E":
+        return require(`../../assets/e.png`);
+      case "Mottu Pop":
+        return require(`../../assets/pop.png`);
+      default:
+        return require(`../../assets/pop.png`);
+    }
+  };
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      backdropColor={"gray"}
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)}
+    >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        <View
+          style={{
+            width: 300,
+            padding: 20,
+            backgroundColor: "#C4C4C4",
+            borderRadius: 10,
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
+              left: 6,
+              top: 6,
+              position: "absolute",
+            }}
+          >
+            <FontAwesome
+              name="close"
+              color="#41C526"
+              size={30}
+              onPress={() => setModalVisible(false)}
+            />
+          </View>
+          <View
+            style={{
+              padding: 20,
+              gap: 15,
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ fontSize: 28, color: "black", fontWeight: "bold" }}>
+              {motoData.nome}
+            </Text>
+            <Text style={{ fontSize: 16, color: "black" }}>
+              {motoData.identificador}
+            </Text>
+            <Text style={{ fontSize: 28, color: "green", fontWeight: "bold" }}>
+              Status
+            </Text>
+            <Text style={{ fontSize: 16, color: "black" }}>
+              {motoData.status}
+            </Text>
+            <Image
+              source={rightName(motoData.nome)}
+              style={{ width: 150, height: 150, alignSelf: "center" }}
+            />
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 10,
+              }}
+            >
+              <ButtonArea
+                size="small"
+                title="Editar"
+                action={() =>
+                  ToastAndroid.show("Está funcionando", ToastAndroid.LONG)
+                }
+              />                     
+              <ButtonArea
+                size="small"
+                title="Localizar"
+                action={() =>
+                  ToastAndroid.show("Está funcionando", ToastAndroid.LONG)
+                }
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
