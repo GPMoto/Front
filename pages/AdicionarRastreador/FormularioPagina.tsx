48ddc02298bceb7d3bce3e88a6bcf78bf2ee1df6
gustavodscruz <gustavodiasdsc@gmail.com>
2025-05-17 18:41:01 -0300

    feat: indo para formulario com sucesso

---
+++ pages/AdicionarRastreador/FormularioPagina.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View } from "react-native";
+import FormularioUwb from "./components/FormularioUwb/FormularioUwb";
+import { styles } from "../../styles/styles";
+
+interface FormularioPaginaProps {
+  identificador: string;
+  setIdentificador: (value: string) => void;
+  setLoading: (value: boolean) => void;
+}
+
+function FormularioPagina({
+  identificador,
+  setIdentificador,
+  setLoading,
+}: FormularioPaginaProps) {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          width: "80%",
+          alignSelf: "center",
+          display: identificador ? "flex" : "none",
+        },
+      ]}
+    >
+      <FormularioUwb
+        identificador={identificador}
+        setIdentificador={setIdentificador}
+        setLoading={setLoading}
+      />
+    </View>
+  );
+}
+
+export default FormularioPagina;
--- pages/AdicionarRastreador/components/FormularioUwb/FormularioUwb.tsx
+++ pages/AdicionarRastreador/components/FormularioUwb/FormularioUwb.tsx
@@ -4,6 +4,7 @@ import InputLabel from '../../../../components/InputLabel/InputLabel';
 import { useState } from 'react';
 import ButtonArea from '../../../../components/Button/ButtonArea';
 import axios from 'axios';
+import { styles } from '../../../../styles/styles';
 
 interface FormularioUwbProps{
     identificador:string;
@@ -34,13 +35,39 @@ export default function FormularioUwb(props:FormularioUwbProps) {
         }
     }
 
+
+    const enviaDadosMock = async () => {
+        if(condicoesManutencao && id){
+            const moto = {condicoesManutencao,idIdentificador:id}
+            const res = await axios.post("url/postMoto",JSON.stringify(moto))
+            if(res.status = 201){
+                ToastAndroid.show("Rastreador adicionado", ToastAndroid.LONG);
+                props.setLoading(false)
+                setCondicoesManutencao("")
+                setId(0);
+                props.setIdentificador("")
+            }else{
+                ToastAndroid.show("Não foi possivel adicionar rastreador.", ToastAndroid.LONG);
+            }
+        }else{
+            ToastAndroid.show("Preencha os campos!", ToastAndroid.LONG);
+        }
+    }
+
+    const voltarCamera = () => {
+        props.setIdentificador("")
+    }
+
   return (
     <View>
-        <Text>Adicionar Informações da moto {props.identificador}</Text>
+        <Text style={[styles.paragraph, {textAlign: 'center'}]}>Adicionar Informações da moto {props.identificador}</Text>
         <View>
             <InputLabel title='Id do rastreador' show={false} setValue={(e)=>setId(parseInt(e))} value={id.toString()} placeholder='Adicionar id do rastreador'></InputLabel>
             <InputLabel title='Condições de manutenção' show={false} setValue={(e)=>setCondicoesManutencao(e)} value={condicoesManutencao} placeholder='Coloque a condição de manutenção'></InputLabel>
-            <ButtonArea size='medium' title='Salvar rastreador' action={()=>enviaDados()}></ButtonArea>
+            <View style={{flexDirection: 'row', gap: 5}}>
+                <ButtonArea additionalStyles={{marginVertical: 10}} size='small' title='Salvar' action={()=>enviaDados()}></ButtonArea>
+                <ButtonArea additionalStyles={{marginVertical: 10 }} size='small' title='Câmera' action={()=>voltarCamera()}></ButtonArea>
+            </View>
         </View>
     </View>
   )
