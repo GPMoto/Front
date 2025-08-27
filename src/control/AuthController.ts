93b4b2355068ecfe07b68d2c0706d215702fdc6a
gustavodscruz <gustavodiasdsc@gmail.com>
2025-08-27 10:23:14 -0300

    refactor: generic response para todos os responses callback

--- src/control/AuthController.ts
+++ src/control/AuthController.ts
@@ -1,10 +1,11 @@
 import { useAuth } from "@/context/AuthContext";
-import { UserLogin } from "@/model/UserLogin";
+import { UserLogin, UserLoginErrors } from "@/model/UserLogin";
 import { AuthService } from "@/services/AuthService";
 import { useState } from "react";
 
 const useAuthControl = () => {
   const [loading, setLoading] = useState<boolean>(false);
+  const [loginErrors, setLoginErrors] = useState<UserLoginErrors>({})
   const { login } = useAuth();
 
   const loginUser = async (userLogin: UserLogin) => {
