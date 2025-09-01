e7ece0b9cf54def33e63b35b55df3fceb7cab31d
gustavodscruz <gustavodiasdsc@gmail.com>
2025-09-01 08:48:56 -0300

    loading screen no inicio da aplicacao

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "@/navigators/RootNavigator";
 import { AuthProvider } from "@/context/AuthContext";
+import SplashApp from "@/navigators/SplashApp";
 
 export default function App() {
   return (
     <AuthProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <SplashApp />
     </AuthProvider>
   );
 }
